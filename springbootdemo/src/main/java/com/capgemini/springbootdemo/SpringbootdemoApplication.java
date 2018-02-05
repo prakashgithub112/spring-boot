b949d8491938e88439244382d30736107aca433c
anushka sharma <anushka sharma@192.168.1.104>
2018-02-05 20:04:22 +0530

    chnages done at home

--- springbootdemo/src/main/java/com/capgemini/springbootdemo/SpringbootdemoApplication.java
+++ springbootdemo/src/main/java/com/capgemini/springbootdemo/SpringbootdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("checked in from eclipse");
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 }
